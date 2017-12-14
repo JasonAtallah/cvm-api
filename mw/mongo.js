13ccc14831cfd7e285818ba1bc5b8486f9cd1e29
Ariel Jakobovits <arieljake@gmail.com>
2017-12-13 19:54:12 -0800

    debug file download

--- mw/mongo.js
+++ mw/mongo.js
@@ -399,21 +399,21 @@ module.exports = {
 
   sendFile(req, res, next) {
     res.set('Content-Type', req.file.contentType);
-    res.set('Content-Disposition', `attachment; filename="${req.file.filename}"`);
+    res.set('Content-Disposition', `attachment; filename="${req.file.originalname}"`);
 
     config.mongo.getDB
     .then((db) => {
       var bucket = new GridFSBucket(db,
         { bucketName: req.file.bucketName, chunkSizeBytes: req.file.chunkSize });
-      var downloadStream = bucket.openDownloadStream(req.file.id);        
-      downloadStream.on("error", function(err) { 
+      var downloadStream = bucket.openDownloadStream(new ObjectID(req.file.id));
+      downloadStream.on("error", function(err) {
         next(err);
       });
-      downloadStream.pipe(res);  
+      downloadStream.pipe(res);
     })
     .catch((err) => {
       next(err);
-    });        
+    });
   },
 
   updateBuyerEmailTemplate(req, res, next) {
