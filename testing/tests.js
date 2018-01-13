22e5b4130c11e2acf4d6f49c8e03d59b21987d78
Jason Atallah <jasonatallah@jasons-mbp.attlocal.net>
2018-01-12 21:56:33 -0800

    reorganized test.js

--- testing/tests.js
+++ testing/tests.js
@@ -5,27 +5,56 @@ const collection = require('./CannabisVendorMgmt.postman_collection.json');
 const env = require('./env');
 const requests = new Requests(collection, env);
 
-describe('buyer/v1', function() {
-  describe('get buyer token', function() {
-    it('should return jwt token', function() {
-      return requests.run('post-token-success')
-        .then((response) => {
-          expect(response.statusCode).to.equal(200);
-          expect(response.body).to.match(/^[\w\W]{100,}$/);
-          env.BUYER_TOKEN = response.body;
-        });
+describe('CVM APIs', function() {
+  describe('buyer/v1', function() {
+    describe('get buyer token', function() {
+      it('should return jwt token', function() {
+        return requests.run('post-token-success')
+          .then((response) => {
+            expect(response.statusCode).to.equal(200);
+            expect(response.body).to.match(/^[\w\W]{100,}$/);
+            env.BUYER_TOKEN = response.body;
+          });
+      });
     });
-  });
+  
+    describe('create vendor', function() {
+      it('should return vendor with right values', function() {
+        return requests.run('post-vendor-success', { vendor: env.vendor1 })
+          .then((response) => {
+            expect(response.statusCode).to.equal(201);
+            expect(response.body.name).to.equal(env.vendor1.company.name);
+            expect(response.body.state).to.deep.include({name: 'NewVendor'});
+            env.VENDOR_ID = response.body._id;
+          });
+      });
+    });
+  });  
 
-  describe('create vendor', function() {
-    it('should return vendor with right values', function() {
-      return requests.run('post-vendor-success', { vendor: env.vendor1 })
-        .then((response) => {
-          expect(response.statusCode).to.equal(201);
-          expect(response.body.name).to.equal(env.vendor1.company.name);
-          expect(response.body.state).to.deep.include({name: 'NewVendor'});
-          env.VENDOR_ID = response.body._id;
-        });
+  describe('vendor/v1', function() {
+    describe('get buyer token', function() {
+      it('should return jwt token', function() {
+        return requests.run('post-token-success')
+          .then((response) => {
+            expect(response.statusCode).to.equal(200);
+            expect(response.body).to.match(/^[\w\W]{100,}$/);
+            env.BUYER_TOKEN = response.body;
+          });
+      });
+    });
+  
+    describe('create vendor', function() {
+      it('should return vendor with right values', function() {
+        return requests.run('post-vendor-success', { vendor: env.vendor1 })
+          .then((response) => {
+            expect(response.statusCode).to.equal(201);
+            expect(response.body.name).to.equal(env.vendor1.company.name);
+            expect(response.body.state).to.deep.include({name: 'NewVendor'});
+            env.VENDOR_ID = response.body._id;
+          });
+      });
     });
   });
-});
+  
+})
+
