e753fd26cd4c06a51137d59c1d12012fdc97f02d
Jason Atallah <jasonatallah@jasons-mbp.attlocal.net>
2018-02-05 20:17:47 -0800

    updated buyer schema and updated event mapping to include location

--- testing/env.js
+++ testing/env.js
@@ -1,11 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-const envJsonPath = path.resolve(__dirname, 'env.json');
-
-if (fs.existsSync(envJsonPath)) {
-  module.exports = require(envJsonPath);
-} else {
-  module.exports = {
-    API_HOST: process.env.TEST_API_HOST
-  }
+module.exports = {
+  API_HOST: process.env.TEST_API_HOST
 }
