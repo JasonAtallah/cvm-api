e9339e0d45c7bb2ba01360e64e3b8d75d1ccf56d
Jason Atallah <jasonatallah@Jasons-MacBook-Pro.local>
2018-07-29 14:22:52 -0700

    fixed failing tests

--- test/api/get/schemas.js
+++ test/api/get/schemas.js
@@ -2,13 +2,6 @@ const context = require('../../../testing/lib/context');
 
 describe('get schemas', function () {
 
-  it('should return 401 Unauthorized without token', function () {
-    return context.requests.run('get-schemas')
-      .then((response) => {
-        context.expect(response.statusCode).to.equal(401);
-      });
-  });
-
   it('should return the schemas', function () {
     const localEnv = {};
 
